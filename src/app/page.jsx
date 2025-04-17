ea748ec536d91c7ad8021af85287d4b1a5455f2e
EA03 <00175420@uca.edu.sv>
2025-04-16 21:03:34 -0600

    :lipstick:Add management table view

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,8 @@
 import AuthCard from '../components/Login/AuthCard';
-import AddCardModel from '@/components/Models/AddCardModel';
 
 export default function LoginPage() {
   return (
-    /*<main 
+    <main 
       className="w-full h-screen bg-cover bg-center flex items-center justify-center p-4"
       style={{ 
         backgroundImage: "url('/Fondo_Login.jpg')",
@@ -11,7 +10,6 @@ export default function LoginPage() {
         backgroundPosition: 'center'
       }}>
       <AuthCard />
-      </main>*/
-      <AddCardModel/>
+      </main>
   );
 }
