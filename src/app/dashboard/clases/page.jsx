13a8f719fda92fe1ff8dd57c87569e4a3ed92d0a
ChristopherCalderon <chris2001289@gmail.com>
2025-04-18 13:54:34 -0600

    feat: add admin protected routes :sparkles:

--- src/app/dashboard/clases/page.jsx
+++ src/app/dashboard/clases/page.jsx
@@ -6,7 +6,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Clases() {
-
+  git add .
 
   return (
     <div className="bg-background flex flex-col gap-5 w-full h-full p-8 overflow-clip">
