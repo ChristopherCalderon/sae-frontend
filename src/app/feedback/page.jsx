ad362131fe66c7612eaf8602484a88d5ac1e94a5
ChristopherCalderon <chris2001289@gmail.com>
2025-05-04 12:17:41 -0600

    feat: add user feedback view :sparkles:

---
+++ src/app/feedback/page.jsx
@@ -0,0 +1,55 @@
+'use client'
+import Loading from '@/components/loader/Loading'
+import React, { useState } from 'react'
+
+import ReactMarkdown from "react-markdown";
+
+function Feedback() {
+    const [loading, setloading] = useState(false);
+  return (
+    <div className="bg-background flex flex-col gap-5 w-full h-screen p-8 overflow-clip">
+    <div className="w-ful text-primary flex items-center justify-between ">
+      <div>
+        <h1 className="text-2xl font-bold">@UserGithub</h1>
+        <p className="font-semibold">Nombre de tarea</p>
+      </div>
+    </div>
+    <div className="w-full h-full flex flex-col gap-4 bg-white shadow-xl overflow-clip px-5 py-5 rounded-md text-primary text-sm">
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className="w-full h-full flex flex-col gap-2">
+          {/* Contenedor de informacion */}
+          <div className="flex w-full justify-between">
+            {/* Informacion de retroalimentacion */}
+            <div className="flex flex-col">
+                
+              <h1 className="font-bold">Email</h1>
+              <h1 className="font-medium">Repositorio</h1>
+              <span className="flex gap-5 font-medium" >
+                <p>
+                  Calificacion:{" "}
+                  <a className="text-accent font-semibold">
+                    0 / 10
+                  </a>
+                </p>
+              </span>
+            </div>
+          </div>
+
+          {/* Contenedor de feedback */}
+          <div
+            className="w-full h-[90%] p-5 rounded-md shadow-md overflow-y-scroll bg-background   [&::-webkit-scrollbar]:w-1
+      [&::-webkit-scrollbar-track]:bg-background
+      [&::-webkit-scrollbar-thumb]:bg-primary"
+          >
+            <ReactMarkdown>Feedback</ReactMarkdown>
+          </div>
+        </div>
+      )}
+    </div>
+  </div> 
+  )
+}
+
+export default Feedback
