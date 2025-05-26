9bcc49b466f49784170c427cdaef1c2cf8bbd73e
ChristopherCalderon <chris2001289@gmail.com>
2025-05-26 13:18:37 -0600

    feat: add responsive manage users table :sparkle:

--- src/app/dashboard/admin/page.jsx
+++ src/app/dashboard/admin/page.jsx
@@ -1,26 +1,48 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import ManagementTable from "@/components/tables/ManagementTable";
-import { getClasses, getTeachers } from "@/services/githubService";
-import { useSession } from "next-auth/react";
 import Loading from "@/components/loader/Loading";
+import OrgUsersTableCard from "@/components/tableCards/OrgUsersTable";
+import UsersTableCard from "@/components/tableCards/UsersTableCard";
+import OrgUsersTable from "@/components/tables/OrgUsersTable";
+import UsersTable from "@/components/tables/UsersTable";
+import {
+  getOrgUsers,
+  getTeachers,
+  updateOrgAdmin,
+  updateUserStatus,
+} from "@/services/githubService";
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
+import { FaRegQuestionCircle } from "react-icons/fa";
+import { FaRegCircleXmark } from "react-icons/fa6";
+import { IoIosArrowDown } from "react-icons/io";
 
 function AdminPage() {
-  const { data: session, status } = useSession();
   const [loading, setLoading] = useState(true);
-  const [teachers, setTeachers] = useState();
-  const [classes, setClasses] = useState();
+  const [organizations, setOrganizations] = useState();
+  const [selectedOrg, setSelectedOrg] = useState();
+  const [users, setUsers] = useState([]);
+  const [showConfirmModal, setShowConfirmModal] = useState(false);
+  const [pendingAction, setPendingAction] = useState({
+    orgId: null,
+    userId: null,
+  });
+
+  const [globalFilter, setGlobalFilter] = useState(""); //Filtro de buscador
+  // Filtrar submissions
+  const filteredUsers = users.filter((user) =>
+    user.name.toLowerCase().startsWith(globalFilter.toLowerCase())
+  );
+
+  console.log(users);
+  const { data: session, status } = useSession(); 
 
   const getData = async (orgId) => {
     try {
       setLoading(true);
       const response = await getTeachers(orgId);
-      const responseClasses = await getClasses(orgId);
-      if (response && responseClasses) {
+      if (response) {
         console.log(response);
-        console.log(responseClasses);
-        setTeachers(response);
-        setClasses(responseClasses.data);
+        setUsers(response);
       }
       setLoading(false);
     } catch (error) {
@@ -28,25 +50,131 @@ function AdminPage() {
     }
   };
 
+  const handleStatusChange = async (orgId, userId, status) => {
+    try {
+      await updateUserStatus(orgId, userId, status);
+      getData(selectedOrg);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleAdminChange = async (orgId, userId) => {
+    // Modal de confirmacion
+    setPendingAction({ orgId, userId });
+    setShowConfirmModal(true);
+  };
+
+  const confirmAdminChange = async () => {
+    try {
+      setShowConfirmModal(false);
+      setLoading(true);
+      await updateOrgAdmin(pendingAction.orgId, pendingAction.userId);
+      await getData(selectedOrg);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    if (status === "authenticated") {
-      getData(session.user.selectedOrgId);
+    if (status === "authenticated" ) {
+      setSelectedOrg(session.user.selectedOrgId)
+      getData(session.user.selectedOrgId)
     } else if (status === "loading") {
       // Sesión aún cargando
       setLoading(true);
     }
-  }, [status]);
+  }, [status]); 
+
+
 
   return (
-    <div className="bg-background flex flex-col gap-5 w-full h-full p-8 ">
-      <div className="w-full text-primary ">
-        <h1 className="text-2xl font-mono font-bold">Administrar secciones</h1>
-        <p className="font-mono">Asigna modelos de IA por secciones</p>
+     <div className="bg-background font-primary font-bold h-full flex flex-col items-center gap-5 w-full p-2 lg:p-5 py-8  lg:overflow-clip">
+      <div className="w-full flex flex-col items-center gap-2  text-primary">
+        <h1 className="text-2xl font-bold text-center">Administrar usuarios</h1>
+        <p className="font-light text-center">
+          Selecciona una organización y administra sus usuarios
+        </p>
       </div>
 
-      <div className="w-full h-11/12 bg-white shadow-xl">
-        {loading ? <Loading /> : <ManagementTable classes={classes} teachers={teachers}/>}
-      </div>
+      {loading ? (
+        <Loading message={"Cargando..."} />
+      ) : (
+        <div className="w-4/5 flex flex-col lg:flex-row lg:w-full lg:justify-center  gap-2 mt-2 ">
+          <input
+            type="text"
+            placeholder="Buscar usuarios..."
+            value={globalFilter}
+            onChange={(e) => setGlobalFilter(e.target.value)}
+            className=" px-3 py-2 border-2 border-none bg-[#dcdcdc] rounded w-full lg:w-[40%] font-normal "
+          />
+        </div>
+      )}
+
+      {users.length === 0 ? (
+        <div
+          className={`bg-white w-4/5 mt-16  h-1/2 lg:w-1/2 flex flex-col gap-1 justify-center items-center p-6 rounded  text-center shadow-[0px_8px_8px_rgba(0,0,0,0.25)] ${
+            loading ? "hidden" : ""
+          }`}
+        >
+          <FaRegCircleXmark className="text-5xl " />
+          <h1 className="text-2xl text-primary font-bold">
+            Usuarios no encontrados
+          </h1>
+        </div>
+      ) : (
+        <div className="w-full  h-[70%] lg:h-full  flex flex-col gap-5 lg:overflow-y-auto overflow-y-scroll">
+          {!loading && (
+            <OrgUsersTable
+              users={filteredUsers}
+              orgId={selectedOrg}
+              handleStatusChange={handleStatusChange}
+              handleAdminChange={handleAdminChange}
+            />
+          )}
+
+          {!loading
+            ? filteredUsers.map((user) => (
+                <OrgUsersTableCard
+                  key={user._id}
+                  user={user}
+                  orgId={selectedOrg}
+                  handleStatusChange={handleStatusChange}
+                  handleAdminChange={handleAdminChange}
+                />
+              ))
+            : ""}
+        </div>
+      )}
+      {showConfirmModal && (
+        <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50 ">
+          <div className="bg-white w-full lg:w-1/4 flex flex-col gap-1 justify-center items-center p-6 rounded  text-center shadow-[0px_8px_8px_rgba(0,0,0,0.25)]">
+            <FaRegQuestionCircle className="text-5xl" />
+            <h1 className="text-2xl text-primary font-bold">
+              Administrar usuarios
+            </h1>
+            <p className="text-primary text-lg font-medium mb-2">
+              ¿Está seguro de transferir permisos de administrador?
+            </p>
+            <div className="w-full flex gap-2 justify-center items-center">
+              <button
+                className="flex w-1/3 lg:w-1/3   items-center justify-center gap-2 font-semibold bg-white border-2 border-secondary text-secondary hover:text-white px-5 hover:bg-secondary py-1 rounded shadow-lg"
+                onClick={() => confirmAdminChange()}
+              >
+                Si
+              </button>
+              <button
+                className="flex w-1/3 lg:w-1/3   items-center justify-center gap-2 font-semibold bg-white border-2 border-secondary text-secondary hover:text-white px-5 hover:bg-secondary py-1 rounded shadow-lg"
+                onClick={() => setShowConfirmModal(false)}
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
